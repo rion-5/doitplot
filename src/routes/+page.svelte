7bf1ff0b253ef852f6e6e30d4833d6d1f66009a0
sklee2 <sklee2@me.com>
2024-04-15 19:56:49 +0900

    my first plot chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import * as Plot from '@observablehq/plot';
+    import * as d3 from 'd3';
+  
+    let div;
+    let data = d3.ticks(-2, 2, 200).map(Math.sin);
+  
+    function onMousemove(event) {
+      const [x, y] = d3.pointer(event);
+      data = data.slice(-200).concat(Math.atan2(x, y));
+    }
+  
+    $: {
+      div?.firstChild?.remove(); // remove old chart, if any
+      div?.append(Plot.lineY(data).plot({grid: true})); // add the new chart
+    }
+  </script>
+  
+  <div on:mousemove={onMousemove} bind:this={div} role="img"></div>
