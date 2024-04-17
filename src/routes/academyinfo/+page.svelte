97ba9523c6f036763cc19ca0965e54d3e2995753
sklee2 <sklee2@me.com>
2024-04-17 20:09:28 +0900

    get foreign students count using academyinfo api

---
+++ src/routes/academyinfo/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    let data;
+  
+    async function fetchData() {
+      const response = await fetch('http://166.104.191.253:8089/academyinfo?year=2023');
+      data = await response.json();
+    }
+  
+    fetchData();
+  </script>
+  
+  <h1>Data:</h1>
+  {#if data}
+    <ul>
+      {#each data as item}
+        <li>{item.학교명} 외국인학생수 : {item.외국인학생수}</li>
+      {/each}
+    </ul>
+  {:else}
+    <p>Loading...</p>
+  {/if}
+  
